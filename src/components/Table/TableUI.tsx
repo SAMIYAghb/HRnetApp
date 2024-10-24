a5b5b3c22be2083023d2185ff792a421547ba435
Samiya <samii-1989@live.com>
2024-10-24 12:45:27 +0200

    "add data fils"

--- src/components/Table/TableUI.tsx
+++ src/components/Table/TableUI.tsx
@@ -127,3 +127,17 @@ const TableUI: React.FC = () => {
 }
 
 export default TableUI
+
+
+
+// columns: [
+//   { title: 'First Name', data: 'firstName' },
+//   { title: 'Last Name', data: 'lastName' },
+//   { title: 'Start Date', data: 'startDate' },
+//   { title: 'Department', data: 'department' },
+//   { title: 'Date of Birth', data: 'dateOfBirth' },
+//   { title: 'Street', data: 'street' },
+//   { title: 'City', data: 'city' },
+//   { title: 'State', data: 'state' },
+//   { title: 'Zip Code', data: 'zipCode' },
+// ]
