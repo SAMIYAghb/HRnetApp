72958e4295c33e9acbbf39cf8a884cc3f9ce6764
Samiya <samii-1989@live.com>
2024-10-24 12:41:27 +0200

    "add employee list page UI & tableUI"

--- src/components/Table/TableUI.tsx
+++ src/components/Table/TableUI.tsx
@@ -1,10 +1,128 @@
-import React from 'react'
+import React from 'react';
+import { Table } from 'antd';
+import type { TableColumnsType, TableProps } from 'antd';
 
-const TableUI = () => {
+interface DataType {
+  key: React.Key;
+  name: string;
+  chinese: number;
+  math: number;
+  english: number;
+}
+
+const columns: TableColumnsType<DataType> = [
+  {
+    title: 'First Name',
+    dataIndex: 'name',
+    sorter: {
+      compare: (a, b) => a.chinese - b.chinese,
+      multiple: 3,
+    },
+  },
+  {
+    title: 'Last Name',
+    dataIndex: 'chinese',
+    sorter: {
+      compare: (a, b) => a.chinese - b.chinese,
+      multiple: 3,
+    },
+  },
+  {
+    title: 'Start Date',
+    dataIndex: 'math',
+    sorter: {
+      compare: (a, b) => a.math - b.math,
+      multiple: 2,
+    },
+  },
+  {
+    title: 'Departement',
+    dataIndex: 'english',
+    sorter: {
+      compare: (a, b) => a.english - b.english,
+      multiple: 1,
+    },
+  },
+  {
+    title: 'Date of Birth',
+    dataIndex: 'english',
+    sorter: {
+      compare: (a, b) => a.english - b.english,
+      multiple: 1,
+    },
+  },
+  {
+    title: 'Street',
+    dataIndex: 'english',
+    sorter: {
+      compare: (a, b) => a.english - b.english,
+      multiple: 1,
+    },
+  },
+  {
+    title: 'City',
+    dataIndex: 'english',
+    sorter: {
+      compare: (a, b) => a.english - b.english,
+      multiple: 1,
+    },
+  },
+  {
+    title: 'State',
+    dataIndex: 'english',
+    sorter: {
+      compare: (a, b) => a.english - b.english,
+      multiple: 1,
+    },
+  },
+  {
+    title: 'Zip Code',
+    dataIndex: 'english',
+    sorter: {
+      compare: (a, b) => a.english - b.english,
+      multiple: 1,
+    },
+  },
+];
+
+const data: DataType[] = [
+  {
+    key: '1',
+    name: 'John Brown',
+    chinese: 98,
+    math: 60,
+    english: 70,
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    chinese: 98,
+    math: 66,
+    english: 89,
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    chinese: 98,
+    math: 90,
+    english: 70,
+  },
+  {
+    key: '4',
+    name: 'Jim Red',
+    chinese: 88,
+    math: 99,
+    english: 89,
+  },
+];
+
+const onChange: TableProps<DataType>['onChange'] = (pagination, filters, sorter, extra) => {
+  console.log('params', pagination, filters, sorter, extra);
+};
+
+const TableUI: React.FC = () => {
   return (
-    <div>
-      
-    </div>
+    <Table<DataType> columns={columns} dataSource={data} onChange={onChange} />
   )
 }
 
--- src/pages/EmployeeList/EmployeeList.tsx
+++ src/pages/EmployeeList/EmployeeList.tsx
@@ -1,10 +1,26 @@
+import { Link } from "react-router-dom";
+import TableUI from "../../components/Table/TableUI";
 
 const EmployeeList = () => {
   return (
-    <div>
-      EmployeeList
-    </div>
-  )
-}
+    <>
+      <div className="title_container">
+        <h1>Current Employees</h1>
+      </div>
+      <div className="container">
+        <div className="table_head">
+          <div className="entries"></div>
+          <div className="search"></div>
+        </div>
+        <TableUI />
+        <div className="pagination">
+          <div className="count"></div>
+          <div className="page_number"></div>
+        </div>
+        <Link to="/">Home</Link>
+      </div>
+    </>
+  );
+};
 
-export default EmployeeList
+export default EmployeeList;
